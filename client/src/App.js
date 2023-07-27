fe1d3d819aa9cf1dfc0bca8fbd19d83969531051
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-07-27 16:38:43 -0300

    Cambio back y front url

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Form from "./Views/Form/Form";
 import Landing from "./Views/Landing/Landing";
 import Detail from "./Views/Detail/Detail";
 import axios from "axios";
-axios.defaults.baseURL="https://pi-dogs.railway.internal/";
+axios.defaults.baseURL="https://https://pi-dogs-production-17aa.up.railway.app/";
 
 function App() {
  
