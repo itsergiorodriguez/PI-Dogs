2e2723fd90e9cf5f26f4ea76fdf03283bfc34e13
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-07-27 13:03:31 -0300

    Modifico api Vars

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
@@ -12,7 +12,9 @@ const {
 // });
 
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+ {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
